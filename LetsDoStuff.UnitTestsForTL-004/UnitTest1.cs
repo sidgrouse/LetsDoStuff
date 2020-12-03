22a3751a64e00c335c1bac5bf258ef049d0e657e
TarasovAA <tarasovaa1997@bk.ru>
2020-12-03 21:59:05 +0300

    TL-011:firstTL-011Commit

---
+++ LetsDoStuff.UnitTestsForTL-004/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace LetsDoStuff.UnitTestsForTL_004
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
