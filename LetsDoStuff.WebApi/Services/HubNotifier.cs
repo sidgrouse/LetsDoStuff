66c832a7c2e7dd114da7465bc70d81c2cac4a3c9
TarasovAA <tarasovaa1997@bk.ru>
2020-12-22 12:32:51 +0300

    AddedIHubNotifier

---
+++ LetsDoStuff.WebApi/Services/HubNotifier.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LetsDoStuff.WebApi.Services.Interfaces
+{
+    public class HubNotifier : IHubNotifier
+    {
+        public void NotifyAboutInvitations()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NotifyAboutNewParticipationRequest()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NotifyAboutOwnerActivitiesAnswering()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LetsDoStuff.WebApi/Services/Interfaces/IHubNotifier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LetsDoStuff.WebApi.Services.Interfaces
+{
+    public interface IHubNotifier
+    {
+        void NotifyAboutNewParticipationRequest();
+
+        void NotifyAboutOwnerActivitiesAnswering();
+
+        void NotifyAboutInvitations();
+    }
+}
